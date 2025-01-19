66965b352bab2346005dc5dfaa8d7f3e827a3326
Ankit180898 <karan180898@gmail.com>
2025-01-19 21:17:11 +0530

    Completed Login functionality.

--- lib/init_dependencies.dart
+++ lib/init_dependencies.dart
@@ -1,8 +1,7 @@
-import 'dart:ffi';
-
 import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
 import 'package:blog_app/features/auth/data/repositories/auth_repository_impl.dart';
 import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:blog_app/features/auth/domain/usecases/user_login.dart';
 import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -51,9 +50,15 @@ void _initAuth() {
       authRepository: serviceLocator(),
     ),
   );
+   serviceLocator.registerFactory(
+    () => UserLogin(
+      authRepository: serviceLocator(),
+    ),
+  );
   serviceLocator.registerLazySingleton(
     () => AuthBloc(
       userSignUp: serviceLocator(),
+      userLogin: serviceLocator()
     ),
   );
 }
